92b6b943bb9d7b957e25804bc47004b276551fe8
NazarUsius <nazar.gadzhalov@gmail.com>
2025-05-12 17:45:30 +0300

    Updated main branch

--- accounts/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
-
-
-def RegisterView(request):
-    form = UserCreationForm()
-    return render(request, 'accounts/register.html', {'form': form})
