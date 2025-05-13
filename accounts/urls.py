73592c2f4332231e5d7226dd716864a80c66d63a
NazarUsius <nazar.gadzhalov@gmail.com>
2025-05-13 15:01:24 +0300

    Added login page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.views import LoginView
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('register', views.RegisterView, name='register'),
+    path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
 ]
