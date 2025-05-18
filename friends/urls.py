f01cd87ef6cb9ca44cf8cc4a2bf4f100262648ed
NazarUsius <nazar.gadzhalov@gmail.com>
2025-05-18 19:42:27 +0300

    Added request accept

--- friends/urls.py
+++ friends/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('list', views.FriendsList, name='friends_list'),
     path('request/<str:second>', views.FriendsRequest, name='friends_request'),
+    path('accept/<str:iniciator>', views.FriendsRequestAccept, name='friends_request_accept'),
+
 ]
--- friends/views.py
+++ friends/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
 
 from accounts.models import CustomUser
-from friends.models import Friends, FriendRequest
+from friends.models import Friends
 
 
 def FriendsList(request):
-    friends = Friends.objects.filter(person_one=request.user)
+    friends = Friends.objects.filter(person_one=request.user, accepted=True)
     context = {
         'friends': friends
     }
@@ -13,5 +13,13 @@ def FriendsList(request):
 
 def FriendsRequest(request, second: str):
     secon = get_object_or_404(CustomUser, username=second)
-    FriendRequest.objects.create(iniciator=request.user, acceptor=secon)
+    Friends.objects.create(person_one=request.user, person_two=secon)
     return redirect("index")
+
+def FriendsRequestAccept(request, iniciator:int):
+    f_request = get_object_or_404(Friends, person_one=iniciator)
+    f_request.accepted = True
+    f_request.save()
+    return redirect("friends_list")
+
+
