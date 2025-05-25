20933789c32a15661bb240cc54dc7942b9c46256
NazarUsius <nazar.gadzhalov@gmail.com>
2025-05-25 19:09:00 +0300

    Added friends delete

--- friends/urls.py
+++ friends/urls.py
@@ -3,10 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('list', views.FriendsList, name='friends_list'),
-    path('request/<str:second>', views.FriendsRequest, name='friends_request'),
-    path('accept/<str:iniciator>', views.FriendsRequestAccept, name='friends_request_accept'),
-    path('decline/<str:iniciator>', views.FriendsRequestDecline, name='friends_request_decline'),
+    path('list', views.friends_list, name='friends_list'),
+    path('request/<str:second>', views.friends_request, name='friends_request'),
+    path('accept/<str:iniciator>', views.friends_request_accept, name='friends_request_accept'),
+    path('decline/<str:iniciator>', views.friends_request_decline, name='friends_request_decline'),
+    path('delete/<int:second>', views.friends_delete, name='friends_delete'),
 
 
 ]
--- friends/views.py
+++ friends/views.py
@@ -4,26 +4,31 @@ from accounts.models import CustomUser
 from friends.models import Friends
 
 
-def FriendsList(request):
+def friends_list(request):
     friends = Friends.objects.filter(person_one=request.user, accepted=True)
     context = {
         'friends': friends
     }
     return render(request, "friends/list.html", context)
 
-def FriendsRequest(request, second: str):
+def friends_request(request, second: str):
     secon = get_object_or_404(CustomUser, username=second)
     Friends.objects.create(person_one=request.user, person_two=secon)
     return redirect("index")
 
-def FriendsRequestAccept(request, iniciator:int):
+def friends_request_accept(request, iniciator:int):
     f_request = get_object_or_404(Friends, person_one=iniciator)
     f_request.accepted = True
     f_request.save()
     return redirect("friends_list")
 
-def FriendsRequestDecline(request, iniciator:int):
+def friends_request_decline(request, iniciator:int):
     f_request = get_object_or_404(Friends, person_one=iniciator)
     f_request.delete()
+    return redirect("friends_list")
+
+def friends_delete(request, second: id):
+    friend = get_object_or_404(Friends, person_two=second)
+    friend.delete()
     return redirect("friends_list")
 
